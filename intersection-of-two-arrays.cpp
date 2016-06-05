4f4f78e9c7b35df65fb6dcaafae4a55465158f83
wittyResry <329110452@qq.com>
2016-06-05 13:44:35 +0800

    daily practice

---
+++ intersection-of-two-arrays.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        vector<int> res;
+        for (int i = 0, j = 0; i < nums1.size() && j < nums2.size();) {
+            if (nums1[i] < nums2[j]) ++i;
+            else if (nums1[i] > nums2[j]) ++j;
+            else {
+                if (res.size() == 0) {
+                    res.push_back(nums1[i]);
+                } else {
+                    if (res[res.size() - 1] != nums1[i]) {
+                        res.push_back(nums1[i]);
+                    }
+                }
+                ++i, ++j;
+            }
+        }
+        return res;
+    }
+};
